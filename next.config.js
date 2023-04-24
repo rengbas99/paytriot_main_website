6311fe114f02b11ac7c5062fc8b12d340247b12b
AnishK97 <anishkulandaisamy@outlook.com>
2023-04-24 16:52:43 +0100

    blog account changes and completed

--- next.config.js
+++ next.config.js
@@ -15,25 +15,26 @@ const nextConfig = {
         source: '/team',
         destination: '/about-us',
         permanent: true
-      }
-      // Path Matching - will match `/old-blog/a`, but not `/old-blog/a/b`
-      // {
-      //   source: '/old-blog/:slug',
-      //   destination: '/news/:slug',
-      //   permanent: false
-      // },
-      // Wildcard Path Matching - will match `/blog/a` and `/blog/a/b`
-      // {
-      //   source: '/blog/:slug*',
-      //   destination: '/news/:slug*',
-      //   permanent: false
-      // },
-      // Regex Path Matching - The regex below will match `/post/123` but not `/post/abc`
-      // {
-      //   source: '/post/:slug(\\d{1,})',
-      //   destination: '/news/:slug',
-      //   permanent: false
-      // }
+      },
+
+      //Path Matching - will match `/old-blog/a`, but not `/old-blog/a/b`
+      {
+         source: '/home/newsdetail/:slug',
+         destination: '/blog/:slug',
+         permanent: false
+      },
+       //Wildcard Path Matching - will match `/blog/a` and `/blog/a/b`
+       {
+         source: '/home/newsdetail/:slug*',
+         destination: '/blog/:slug*',
+         permanent: false
+       },
+       //Regex Path Matching - The regex below will match `/post/123` but not `/post/abc`
+       {
+         source: '/post/:slug(\\d{1,})',
+         destination: '/news/:slug',
+         permanent: false
+        }
     ];
   },
 
