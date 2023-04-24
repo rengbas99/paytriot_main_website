58f9310c2dca144d08882616445b20029e966b0c
AnishK97 <anishkulandaisamy@outlook.com>
2023-04-24 11:11:59 +0100

    blog updates and corrections

--- next.config.js
+++ next.config.js
@@ -55,6 +55,8 @@ const nextConfig = {
 
   env: {
     Bearer_Token: process.env.Bearer_Token,
+    CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
+    CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN
   },
   
   async headers() {
--- src/components/landing-page/HeroComponent.tsx
+++ src/components/landing-page/HeroComponent.tsx
@@ -11,7 +11,7 @@ export default function HeroComponent() {
         <div className="row">
           <div className="col-lg-6 ps-lg-5 d-flex flex-column justify-content-center align-items-center mb-5 mb-lg-0">
             <div className="text-wrap d-flex flex-column align-items-lg-start align-items-center">
-              <h1 className="mb-lg-5 mb-4 text-center text-lg-start hero-component"><span className="gradient-text ">Rates from 0% </span>   Monthly Flat Fee, Stop paying per transaction</h1>
+              <h1 className="mb-lg-5 mb-4 text-center text-lg-start hero-component"><span className="gradient-text ">Rates from 0% </span>   Monthly Flat Fee Stop paying per transaction</h1>
               <p className="p-18 mb-5 text-center text-lg-start">
               Paytriot now offers a monthly fee for businesses looking to start processing or switching. 
               </p>
