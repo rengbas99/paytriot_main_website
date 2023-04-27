331c99c11ebf19861e84e76ec05ad41b1aef5c8c
AnishK97 <anishkulandaisamy@outlook.com>
2023-04-27 13:29:02 +0100

    Minor content fix

--- next.config.js
+++ next.config.js
@@ -53,6 +53,7 @@ const nextConfig = {
     CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
     CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN
   },
+
   
   async headers() {
     return [
