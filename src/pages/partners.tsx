f26e2b68e84916d9568203feeed637eb72076d3f
AnishK97 <anishkulandaisamy@outlook.com>
2023-07-20 16:14:15 +0100

    Partner Update HyperLinks

--- src/pages/partners.tsx
+++ src/pages/partners.tsx
@@ -2,6 +2,7 @@ import AgentProgramComponent from '@/components/partner-page/AgentProgram';
 import ReferralProgramComponent from '@/components/partner-page/ReferralProgram';
 import Head from 'next/head';
 import Image from 'next/image';
+import { Link } from '@nextui-org/react';
 
 import imgGambioLogo from 'public/images/img-logo-gambio.png';
 import imgHamptonLogo from 'public/images/img-logo-hampton.png';
@@ -20,7 +21,7 @@ export default function Partners() {
         {/* Required meta tags */}
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Paytriot Payments Partners"/>
+        <meta name="description" content="Paytriot Payments Partners" />
         <title>Partners | Paytriot</title>
       </Head>
       <main>
@@ -28,73 +29,100 @@ export default function Partners() {
           <div className="container">
             <h1 className="text-center">Paytriot Partners</h1>
             <p className="p-18 mt-4 text-center">
-              Great companies we work with 
+              Great companies we work with
             </p>
             <div className="row">
               <div className="col-lg-4 mb-4">
                 <div className="partners-box d-flex flex-column align-items-center">
-                  <Image
-                    width={'80'}
-                    src={imgLogoShopify}
-                    alt=""
-                    className="logo-partners"
-                  />
+                  <Link
+                    style={{ textDecoration: 'none' }}
+                    href={'https://shopify.com/'}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <Image
+                      width={'80'}
+                      src={imgLogoShopify}
+                      alt=""
+                      className="logo-partners"
+                    />
+                  </Link>
                   <label className="partners-txt">Shopify</label>
-                  <p className="p-14 text-opacity">
-                    Global Commerce Platform
-                  </p>
+                  <p className="p-14 text-opacity">Global Commerce Platform</p>
                 </div>
               </div>
               <div className="col-lg-4 mb-4">
                 <div className="partners-box d-flex flex-column align-items-center">
-                  <Image
-                    width={'80'}
-                    src={imgGambioLogo}
-                    alt=""
-                    className="logo-partners"
-                  />
+                <Link
+                    style={{ textDecoration: 'none' }}
+                    href={'https://www.gambio.com/'}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <Image
+                      width={'80'}
+                      src={imgGambioLogo}
+                      alt=""
+                      className="logo-partners"
+                    />
+                </Link>
                   <label className="partners-txt">Gambio</label>
-                  <p className="p-14 text-opacity">
-                    Ecommerce Platform
-                  </p>
+                  <p className="p-14 text-opacity">Ecommerce Platform</p>
                 </div>
               </div>
               <div className="col-lg-4 mb-4">
                 <div className="partners-box d-flex flex-column align-items-center">
+                <Link
+                    style={{ textDecoration: 'none' }}
+                    href={'https://www.hilton.com/en/brands/hampton-by-hilton/'}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
                   <Image
                     width={'80'}
                     src={imgHamptonLogo}
                     alt=""
                     className="logo-partners"
                   />
+                </Link>
                   <label className="partners-txt">Hampton Inn</label>
-                  <p className="p-14 text-opacity">
-                    Leading Hotel Brand
-                  </p>
+                  <p className="p-14 text-opacity">Leading Hotel Brand</p>
                 </div>
               </div>
               <div className="col-lg-4 mb-4">
                 <div className="partners-box d-flex flex-column align-items-center">
+                <Link
+                    style={{ textDecoration: 'none' }}
+                    href={'https://www.matraasia.com/'}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
                   <Image
                     width={'80'}
                     src={imgMatraLogo}
                     alt=""
                     className="logo-partners"
                   />
+                </Link>
                   <label className="partners-txt">Matra Asia</label>
-                  <p className="p-14 text-opacity">
-                    Agricultural Platform
-                  </p>
+                  <p className="p-14 text-opacity">Agricultural Platform</p>
                 </div>
               </div>
               <div className="col-lg-4 mb-4">
                 <div className="partners-box d-flex flex-column align-items-center">
+                <Link
+                    style={{ textDecoration: 'none' }}
+                    href={'https://www.pizzahut.co.uk/'}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
                   <Image
                     width={'80'}
                     src={imgPizzaLogo}
                     alt=""
                     className="logo-partners"
                   />
+                </Link>
                   <label className="partners-txt">Pizza Hut</label>
                   <p className="p-14 text-opacity">
                     Franchise Restaurant Industry
@@ -102,7 +130,13 @@ export default function Partners() {
                 </div>
               </div>
               <div className="col-lg-4 mb-4">
-                <div className="partners-box d-flex flex-column align-items-center">
+                <div className="partners-box d-flex flex-column align-items-center">   
+                <Link
+                    style={{ textDecoration: 'none' }}
+                    href={'https://www.cudedesign.co.uk/'}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
                   <Image
                     width={'200'}
                     height={'200'}
@@ -110,52 +144,66 @@ export default function Partners() {
                     alt=""
                     className="logo-partners"
                   />
+                </Link>
                   <label className="partners-txt">Cude Design</label>
-                  <p className="p-14 text-opacity">
-                    Web Design Agency
-                  </p>
+                  <p className="p-14 text-opacity">Web Design Agency</p>
                 </div>
               </div>
               <div className="col-lg-4 mb-4">
                 <div className="partners-box d-flex flex-column align-items-center">
+                <Link
+                    style={{ textDecoration: 'none' }}
+                    href={'https://www.paypal.com/uk/home'}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
                   <Image
                     width={'80'}
                     src={imgPaypalLogo}
                     alt=""
                     className="logo-partners"
                   />
+                </Link>
                   <label className="partners-txt">Paypal</label>
-                  <p className="p-14 text-opacity">
-                    Online Digital Wallet
-                  </p>
+                  <p className="p-14 text-opacity">Online Digital Wallet</p>
                 </div>
               </div>
               <div className="col-lg-4 mb-4">
                 <div className="partners-box d-flex flex-column align-items-center">
+                <Link
+                    style={{ textDecoration: 'none' }}
+                    href={'https://jumpseller.com/'}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
                   <Image
                     width={'80'}
                     src={imgLogoJumpSellerLogo}
                     alt=""
                     className="logo-partners"
                   />
+                </Link>
                   <label className="partners-txt">Jump Seller</label>
-                  <p className="p-14 text-opacity">
-                    Ecommerce Platform
-                  </p>
+                  <p className="p-14 text-opacity">Ecommerce Platform</p>
                 </div>
               </div>
               <div className="col-lg-4 mb-4">
                 <div className="partners-box d-flex flex-column align-items-center">
+                <Link
+                    style={{ textDecoration: 'none' }}
+                    href={'https://luisonofre.com/'}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
                   <Image
                     width={'80'}
                     src={imgLuisLogo}
                     alt=""
                     className="logo-partners"
                   />
+                </Link>
                   <label className="partners-txt">Luis Onofre</label>
-                  <p className="p-14 text-opacity">
-                    Online Retail Platform
-                  </p>
+                  <p className="p-14 text-opacity">Online Retail Platform</p>
                 </div>
               </div>
             </div>
